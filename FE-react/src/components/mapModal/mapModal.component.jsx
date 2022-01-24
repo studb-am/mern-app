a7a8cd991b5f93f330f5e16494716daa47154a17
stubdAM <61664072+stubdAM@users.noreply.github.com>
2022-01-24 18:53:35 +0100

    switched the map component from internal to my source

--- FE-react/src/components/mapModal/mapModal.component.jsx
+++ FE-react/src/components/mapModal/mapModal.component.jsx
@@ -6,18 +6,37 @@ import {
   Button,
 } from '@mui/material';
 
-import {StyledCard, StyledCardHeader} from './mapModal.styles';
-import Map from '../map/map.component';
+import { StyledCard, StyledCardHeader } from './mapModal.styles';
+import { Map, Marker } from 'open-map-gl';
+
+import './mapModal.css';
 
 const MapModal = props => {
-  const {open, onClose, title, location} = props;
+  const { open, onClose, title, location } = props;
 
   return (
     <MuiModal open={open} onClose={onClose}>
       <StyledCard>
         <StyledCardHeader title={title} />
-        <CardContent sx={{ height: '95%'}}>
-          <Map location={location} />
+        <CardContent sx={{ height: '95%' }}>
+          <Map
+            centerCoords={{ lat: location.lat, lng: location.lng }}
+            initialZoom={14}
+            mapStyle='http://locomovolt.com:8080/styles/basic-preview/style.json'
+            mapClassName="map"
+            mapContainerClassName="map-wrap"
+            minZoom={9}
+            maxZoom={18}
+            navigationControl="top-right"
+          >
+            <Marker
+              coords={{ lat: location.lat, lng: location.lng }}
+              options={{
+                color: '#3FB1CE',
+                draggable: false
+              }}
+            />
+          </Map>
           <Box padding={3} textAlign="center">
             <Button variant="contained" onClick={onClose}>Close</Button>
           </Box>
