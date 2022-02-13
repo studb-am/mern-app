b1924de1f7f4c5c1b1ea5987b8fff289a852dccf
root <root@_HOSTNAME_>
2022-02-13 19:06:14 +0100

    changed the map token validation

--- BE-nodeExpress/controllers/map-controllers.js
+++ BE-nodeExpress/controllers/map-controllers.js
@@ -1,9 +1,15 @@
 const HttpError = require('../models/http-error');
 
+//This function works with a get request (where params have been appended to the url)
 const validateToken = (req, res, next) => {
-   res.status(200).json({ 'message': 'authorized!'});
-   //return next(new HttpError('unauthorized!', 401));
+	const token = req.query.access_token;
+	if (token !== 'abc') {
+		return next(new HttpError('unauthorized', 401));
+	}
+	
+	res.status(200).json({ 'message': 'authorized!'});
 }
+
 
 module.exports = {
     validateToken
